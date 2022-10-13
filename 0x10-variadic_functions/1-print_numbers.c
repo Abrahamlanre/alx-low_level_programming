88c89a25b8a52cf52633724cb008dae7c849a9f8
Abrahamlanre <lanreabraham44@gmail.com>
2022-10-13 04:45:26 -0700

    0x10-variadic_functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ return;
 }
 if (separator == 0)
 separator = "";
-va_start(list, n)
+va_start(list, n);
 for (i = 0; i < n - 1; i++)
 printf("%d%s", va_arg(list, int), separator);
 printf("%d\n", va_arg(list, int));
